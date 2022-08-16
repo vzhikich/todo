661b4d95c53ec13b356314baf8ffb91432913fce
Yevhenii Flyk <yevhenii.flyk.work@gmail.com>
2022-08-16 14:51:58 +0300

    create_model

---
+++ lib/feature/data/model/task.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+Task taskFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Task.fromJson(jsonData);
+}
+
+String taskToJson(Task data) {
+  final dyn = data.toJson();
+  return json.encode(dyn);
+}
+
+class Task extends Equatable {
+  final int id;
+  final String text;
+  final bool checked;
+
+  const Task({required this.id, required this.text, required this.checked});
+
+  @override
+  List<Object?> get props => [id, text, checked];
+
+  factory Task.fromJson(Map<String, dynamic> json) => Task(
+        id: json["id"],
+        text: json["task"],
+        checked: json["checked"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "task": text,
+        "checked": checked,
+      };
+}
