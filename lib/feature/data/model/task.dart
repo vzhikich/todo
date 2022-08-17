72893e0946faf3ede5b9852eef498dbaf3cc47f0
Yevhenii Flyk <yevhenii.flyk.work@gmail.com>
2022-08-17 17:54:16 +0300

    Update task.dart

--- lib/feature/data/model/task.dart
+++ lib/feature/data/model/task.dart
@@ -4,25 +4,25 @@ import 'package:equatable/equatable.dart';
 
 Task taskFromJson(String str) {
   final jsonData = json.decode(str);
-  return Task.fromJson(jsonData);
+  return Tasks.fromJson(jsonData);
 }
 
-String taskToJson(Task data) {
+String taskToJson(Tasks data) {
   final res = data.toJson();
   return json.encode(res);
 }
 
-class Task extends Equatable {
+class Tasks extends Equatable {
   final int id;
   final String text;
   final bool checked;
 
-  const Task({required this.id, required this.text, required this.checked});
+  const Tasks({required this.id, required this.text, required this.checked});
 
   @override
   List<Object?> get props => [id, text, checked];
 
-  factory Task.fromJson(Map<String, dynamic> json) => Task(
+  factory Tasks.fromJson(Map<String, dynamic> json) => Tasks(
         id: json["id"],
         text: json["task"],
         checked: json["checked"],
